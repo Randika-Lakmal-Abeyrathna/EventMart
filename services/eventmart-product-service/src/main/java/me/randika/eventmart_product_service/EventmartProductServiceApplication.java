b564a3612d914ce140c08ada92dfb6ba295d81f0
Randika Lakmal <35038832+Randika-Lakmal-Abeyrathna@users.noreply.github.com>
2025-11-20 21:18:46 +0530

    feat: Initialize product service with Maven setup and basic configuration (#7)
    Co-authored-by: Randika Lakmal <randika.l.abeyrathna@gmail.com>

---
+++ services/eventmart-product-service/src/main/java/me/randika/eventmart_product_service/EventmartProductServiceApplication.java
@@ -0,0 +1,13 @@
+package me.randika.eventmart_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmartProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmartProductServiceApplication.class, args);
+	}
+
+}
---
+++ services/eventmart-product-service/src/test/java/me/randika/eventmart_product_service/EventmartProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.randika.eventmart_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmartProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
