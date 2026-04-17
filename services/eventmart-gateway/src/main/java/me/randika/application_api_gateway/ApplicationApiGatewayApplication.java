db0bab1ef3627fd6b69fc70cbcf9a1c69201bcd4
Randika Lakmal <35038832+Randika-Lakmal-Abeyrathna@users.noreply.github.com>
2026-04-17 22:55:12 +0530

    init commit (#54)
    * init commit
    
    * added docker and change the yml file for routes

---
+++ services/eventmart-gateway/src/main/java/me/randika/application_api_gateway/ApplicationApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package me.randika.application_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationApiGatewayApplication.class, args);
+	}
+
+}
